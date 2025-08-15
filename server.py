a359339a6e5d7d43ca3826fbd12c8b210e48e0e4
Karina-SaRi19 <2022371122@uteq.edu.mx>
2025-08-15 07:05:07 -0600

    Update server.py

--- server.py
+++ server.py
@@ -2,9 +2,13 @@ from flask import Flask, send_file
 
 app = Flask(__name__)
 
+# Ruta principal para evitar 404 en Render
+@app.route('/')
+def index():
+    return "Servidor OTA funcionando 🎉"
+
+# Ruta que sirve tu archivo OTA
 @app.route('/simple_ota.bin')
 def serve_bin():
     return send_file('simple_ota.bin', as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
